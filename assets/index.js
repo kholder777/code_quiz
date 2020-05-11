384eaa32e74cbfe8bcaf13d277f6539a2e07d539
Kevin Holder <kholder777@yahoo.com>
2020-05-11 14:30:20 -0500

    Timer complete. Also added the code to take away time for wrong answers. That section is commented out. This closes #17.

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,5 @@
 // Create all necessary elements and tie them to a variable.
-var h1EL = document.createElement("h1");
+
 var h2EL = document.createElement("h2");
 var h3EL = document.createElement("h3");
 var pEL = document.createElement("p");
@@ -9,6 +9,7 @@ var li1 = document.createElement("li");
 var li2 = document.createElement("li");
 var li3 = document.createElement("li");
 
+var stBtn = document.getElementById("stBtn");
 var btnEL = document.createElement("button");
 var aBtnEL = document.querySelector("buttonA");
 var bBtnEL2 = document.querySelector("buttonB");
@@ -20,48 +21,92 @@ var divItem = document.getElementsByTagName("div");
 
 var timerEl = document.querySelector("#timer");
 var MainBodyEL = document.getElementById("mainBody");
+
 //
 //
-var remainingTime = 2;
+const startBtn = document.getElementById("stBtn");
+const nextBtn = document.getElementById("nxtBtn");
+const questionContainerEL = document.getElementById("question-container");
+const questionEL = document.getElementById("question");
+const answerButtonsEL = document.getElementById("answerButtons");
+//
+var shuffledQuestions, currentQuestionIndex;
 
-function setTime() {
-	var timerInterval = setInterval(function () {
-		remainingTime = remainingTime - 1;
-		timerEl.textContent = "You only have " + remainingTime + " seconds left!";
+startBtn.addEventListener("click", startGame);
+//
+function startGame() {}
 
-		if (remainingTime === 0) {
-			clearInterval(timerInterval);
-			// sendMessage("Game Over");
-		}
-	}, 1000);
-}
+// add event listener for start quiz button (#stBtn).
+//
+stBtn.addEventListener("click", function () {
+	setTime();
 
-// function sendMessage() {
-// 	timerEl.textContent = " ";
-// 	// this is how to append the main body whenever the time finally runs out.
-// 	MainBodyEL.appendChild(variableHere);
+	// start game
+	//
+	// append body div material and populate first question.
+	var remainingTime = 2;
 
-// 	var variableHere = document.createElement("Node", "not a number!");
-// }
+	function setTime() {
+		var timerInterval = setInterval(function () {
+			remainingTime = remainingTime - 1;
+			timerEl.textContent = "You have " + remainingTime + " seconds remaining.";
 
-setTime();
-// Append all the elements
-
-// Style all the elements
+			if (remainingTime === 0) {
+				clearInterval(timerInterval);
+				return "Game Over";
+			}
+		}, 1000);
+	}
+});
 
 // -----------questions below this line------------
-// 1) What is not a way to create a pop-up in JS?
-// x)surprise b)prompt c)alert d)confirm
+const questions = [
+	{
+		prompt:
+			"What is not a way to create a pop-up in JS?\n (a)surprise (b)prompt (c)alert (d)confirm",
+		answer: "a",
+	},
+	{
+		prompt:
+			"To ________ is to increase a number by one.\n a)add b)increment c)increase d)amplify",
+		answer: "b",
+	},
+	{
+		prompt:
+			"To ________ is to decrease a number by one.\n a)subtract b)decrease c)decrement d)minus",
+		answer: "c",
+	},
+	{
+		prompt:
+			"Make this run, only if they are both true. (example === 5 ____ example === 10)\n a)&& b)&> c)&| d)&=",
+		answer: "a",
+	},
+];
+// var score = 0;
+
+// for(var i=0; i < questions.length; i++){
+// 	var response = window.prompt(questions[i].prompt);
+// 	if (response === questions[i].answer){
+// 		score++;
+// 		return("Correct!")
+// 	} else () {
+// 		timer-10;
+// 		return("Incorrect")
+// 	}
+// 	// else if needs -5 seconds.
+// }
+
 //
-// 2) To ________ is to increase a number by one.
-// x)increment b)add c)increase d)amplify
 //
-// 3) To ________ is to decrease a number by one.
-// x)decrement b)subtract c)decrease d)minus
 //
-// 4) Make this run, only if they are both true. (example === 5 ____ example === 10)
-// x)&& b)&> c)&| d)&=
 //
+//
+//
+//
+//
+//
+//
+// ------------------remaining questions------------
 // 5) Make this run, if at least one is true. (example === 5 ____ example === 10)
 // x)|| b)\\ c)-- d)><
 //
@@ -79,9 +124,3 @@ setTime();
 //
 // 10)
 //
-//
-//
-//
-//
-// Set the body to a variable.
-// var body = document.body;
