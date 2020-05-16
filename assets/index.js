c268e0ef9bbb18c1811d8f0e2112756b6cc8685f
Ashley Lerma <lerma.scout@gmail.com>
2020-05-15 19:05:53 -0500

    local storage debugged

--- assets/index.js
+++ assets/index.js
@@ -22,36 +22,36 @@ submit.style.display = "none";
 userName.style.display = "none";
 // check answer function
 function checkAnswer() {
-	var choice = this.getAttribute("data-value");
-	// console.log(runningQuestionIndex);
-	let q = questions[runningQuestionIndex];
-	if (choice === q.answer) {
-		remainingTime = remainingTime + 3;
-		score++;
-		console.log("score is:", score);
-	} else {
-		remainingTime = remainingTime - 7;
-	}
-	scoreBoardEL.innerText = score;
-	runningQuestionIndex++;
-	if (runningQuestionIndex <= questions.length) {
-		generateQuestion();
-	}
+  var choice = this.getAttribute("data-value");
+  // console.log(runningQuestionIndex);
+  let q = questions[runningQuestionIndex];
+  if (choice === q.answer) {
+    remainingTime = remainingTime + 3;
+    score++;
+    console.log("score is:", score);
+  } else {
+    remainingTime = remainingTime - 7;
+  }
+  scoreBoardEL.innerText = score;
+  runningQuestionIndex++;
+  if (runningQuestionIndex <= questions.length) {
+    generateQuestion();
+  }
 }
 // start game button/function
 stBtnEL.addEventListener("click", function () {
-	generateQuestion();
-	setTime();
-	stBtnEL.outerHTML = "";
+  generateQuestion();
+  setTime();
+  stBtnEL.outerHTML = "";
 });
 // create newestSuperScore generator
 function generateQuestion() {
-	let q = questions[runningQuestionIndex];
-	questionEL.innerHTML = q.question;
-	buttonAEL.innerHTML = q.aButtonEL;
-	buttonBEL.innerHTML = q.bButtonEL;
-	buttonCEL.innerHTML = q.cButtonEL;
-	buttonDEL.innerHTML = q.dButtonEL;
+  let q = questions[runningQuestionIndex];
+  questionEL.innerHTML = q.question;
+  buttonAEL.innerHTML = q.aButtonEL;
+  buttonBEL.innerHTML = q.bButtonEL;
+  buttonCEL.innerHTML = q.cButtonEL;
+  buttonDEL.innerHTML = q.dButtonEL;
 }
 
 buttonAEL.addEventListener("click", checkAnswer);
@@ -60,65 +60,67 @@ buttonCEL.addEventListener("click", checkAnswer);
 buttonDEL.addEventListener("click", checkAnswer);
 // timer
 function setTime() {
-	var timerInterval = setInterval(function () {
-		remainingTime -= 1;
-		timerEl.textContent = "You have " + remainingTime + " seconds remaining.";
-		if (remainingTime <= 0) {
-			clearInterval(timerInterval);
-			scoreBoardEL.textContent = score;
-			timerEl.textContent = "Game Over, man.";
-			questionEL.textContent = "";
-			buttonAEL.outerHTML = "";
-			buttonBEL.outerHTML = "";
-			buttonCEL.outerHTML = "";
-			buttonDEL.outerHTML = "";
-			// stBtnEL.outerHTML = "";
-			runningQuestionIndex = 0;
-			// gameOver();
-			// completeQuiz(score);
-		}
-	}, 1000);
+  var timerInterval = setInterval(function () {
+    remainingTime -= 1;
+    timerEl.textContent = "You have " + remainingTime + " seconds remaining.";
+    if (remainingTime <= 0) {
+      clearInterval(timerInterval);
+      scoreBoardEL.textContent = score;
+      timerEl.textContent = "Game Over, man.";
+      questionEL.textContent = "";
+      buttonAEL.outerHTML = "";
+      buttonBEL.outerHTML = "";
+      buttonCEL.outerHTML = "";
+      buttonDEL.outerHTML = "";
+      // stBtnEL.outerHTML = "";
+      runningQuestionIndex = 0;
+      // gameOver();
+      completeQuiz();
+    }
+  }, 1000);
 }
 
 // -----------------------------------------------------------------------------------------------------------semi-working code-----------------
-function completeQuiz(score, initials) {
-	// instructions.setAttribute("class", "hide");
-	// quizCont.setAttribute("class", "hide");
-	// endGame.removeAttribute("class", "hide");
-	// initials.removeAttribute("class", "hide");
-	// submit.style.display = "block";
-	userName.style.display = "block";
+function completeQuiz() {
+  //   instructions.setAttribute("class", "hide");
+  //   quizCont.setAttribute("class", "hide");
+  //   endGame.removeAttribute("class", "hide");
+  //   initials.removeAttribute("class", "hide");
+  submit.style.display = "block";
+  userName.style.display = "block";
 
-	// needs modified..............
-	var button = document.createElement("button");
-	button.innerText = "Save Score";
-	button.addEventListener("click", function (event) {
-		var Scores = JSON.parse(localStorage.getItem("Scores")) || [];
-		console.log(Scores);
+  // needs modified..............
+  //   var button = document.createElement("button");
+  //   button.innerText = "Save Score";
+  submit.addEventListener("click", function (event) {
+    event.preventDefault();
+    var Scores = JSON.parse(localStorage.getItem("Scores")) || [];
+    console.log(Scores);
 
-		Scores.push({
-			initials: document.getElementById("initials").value,
-			points: score,
-		});
-		console.log(Scores);
-		localStorage.setItem("Scores", JSON.stringify(Scores));
+    var initials = userName.value + ": " + score;
 
-		initials.setAttribute("class", "hide");
-		document.getElementById("Scores").setAttribute("class", "hide");
+    Scores.push(initials);
+    console.log(Scores);
+    localStorage.setItem("Scores", JSON.stringify(Scores));
 
-		for (var i = 0; i < Scores.length; i++) {
-			var li = document.createElement("li");
-			li.innerHTML = Scores[i].initials + " " + Scores[i].points;
-			document.getElementById("highScores").append(li);
-		}
-		// why not displaying???
-	}); // save the player's score
-	// highScoreEL.append(button);
-	console.log("highScores");
+    // initials.setAttribute("class", "hide");
+    // document.getElementById("Scores").setAttribute("class", "hide");
 
-	// save the scores back in localstorage
+    for (var i = 0; i < Scores.length; i++) {
+      var li = document.createElement("li");
+      li.innerHTML = Scores[i];
 
-	// display the scoreboard
+      document.getElementById("highScores").append(li);
+    }
+    userName.value = " ";
+    // why not displaying???
+  }); // save the player's score
+  // highScoreEL.append(button);
+  console.log("highScores");
+
+  // save the scores back in localstorage
+
+  // display the scoreboard
 }
 // ----------------------------------------------------------------------------------------------------------------------------------------
 // function gameOver() {
@@ -163,79 +165,79 @@ function completeQuiz(score, initials) {
 
 // -----------questions below this line------------
 var questions = [
-	{
-		question: "What is NOT a way to create a pop-up in JS?",
-		aButtonEL: "surprise",
-		bButtonEL: "prompt",
-		cButtonEL: "alert",
-		dButtonEL: "confirm",
-		answer: "a",
-	},
-	{
-		question: "To ________ is to increase a number by one.",
-		aButtonEL: "add",
-		bButtonEL: "increment",
-		cButtonEL: "increase",
-		dButtonEL: "amplify",
-		answer: "b",
-	},
-	{
-		question: "To ________ is to decrease a number by one.",
-		aButtonEL: "subtract",
-		bButtonEL: "decrease",
-		cButtonEL: "decrement",
-		dButtonEL: "minus",
-		answer: "c",
-	},
-	{
-		question:
-			"Make this run, only if they are both true. (example === 5 ____ example === 10)",
-		aButtonEL: "&&",
-		bButtonEL: "&>",
-		cButtonEL: "&|",
-		dButtonEL: "&=",
-		answer: "a",
-	},
-	{
-		question:
-			"Make this run, if at least one is true. (example === 5 ____ example === 10)",
-		aButtonEL: "||",
-		bButtonEL: "//",
-		cButtonEL: "--",
-		dButtonEL: "><",
-		answer: "a",
-	},
-	{
-		question:
-			"The ______ statement is used to perform different actions based on different conditions.",
-		aButtonEL: "jump",
-		bButtonEL: "decide",
-		cButtonEL: "action",
-		dButtonEL: "switch",
-		answer: "d",
-	},
-	{
-		question: 'The ______ statement "jumps out" of a loop.',
-		aButtonEL: "append",
-		bButtonEL: "stop",
-		cButtonEL: "break",
-		dButtonEL: "quit",
-		answer: "c",
-	},
-	{
-		question: 'The ______ statement "jumps over" one iteration in the loop.',
-		aButtonEL: "continue",
-		bButtonEL: "skip",
-		cButtonEL: "jump",
-		dButtonEL: "return",
-		answer: "a",
-	},
-	{
-		question: "A JavaScript Boolean represents one of two values:",
-		aButtonEL: "UP / DOWN",
-		bButtonEL: "YES / NO",
-		cButtonEL: "ON / OFF",
-		dButtonEL: "TRUE / FALSE",
-		answer: "d",
-	},
+  {
+    question: "What is NOT a way to create a pop-up in JS?",
+    aButtonEL: "surprise",
+    bButtonEL: "prompt",
+    cButtonEL: "alert",
+    dButtonEL: "confirm",
+    answer: "a",
+  },
+  {
+    question: "To ________ is to increase a number by one.",
+    aButtonEL: "add",
+    bButtonEL: "increment",
+    cButtonEL: "increase",
+    dButtonEL: "amplify",
+    answer: "b",
+  },
+  {
+    question: "To ________ is to decrease a number by one.",
+    aButtonEL: "subtract",
+    bButtonEL: "decrease",
+    cButtonEL: "decrement",
+    dButtonEL: "minus",
+    answer: "c",
+  },
+  {
+    question:
+      "Make this run, only if they are both true. (example === 5 ____ example === 10)",
+    aButtonEL: "&&",
+    bButtonEL: "&>",
+    cButtonEL: "&|",
+    dButtonEL: "&=",
+    answer: "a",
+  },
+  {
+    question:
+      "Make this run, if at least one is true. (example === 5 ____ example === 10)",
+    aButtonEL: "||",
+    bButtonEL: "//",
+    cButtonEL: "--",
+    dButtonEL: "><",
+    answer: "a",
+  },
+  {
+    question:
+      "The ______ statement is used to perform different actions based on different conditions.",
+    aButtonEL: "jump",
+    bButtonEL: "decide",
+    cButtonEL: "action",
+    dButtonEL: "switch",
+    answer: "d",
+  },
+  {
+    question: 'The ______ statement "jumps out" of a loop.',
+    aButtonEL: "append",
+    bButtonEL: "stop",
+    cButtonEL: "break",
+    dButtonEL: "quit",
+    answer: "c",
+  },
+  {
+    question: 'The ______ statement "jumps over" one iteration in the loop.',
+    aButtonEL: "continue",
+    bButtonEL: "skip",
+    cButtonEL: "jump",
+    dButtonEL: "return",
+    answer: "a",
+  },
+  {
+    question: "A JavaScript Boolean represents one of two values:",
+    aButtonEL: "UP / DOWN",
+    bButtonEL: "YES / NO",
+    cButtonEL: "ON / OFF",
+    dButtonEL: "TRUE / FALSE",
+    answer: "d",
+  },
 ];
